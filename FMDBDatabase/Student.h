bb1878c43623f75927315e62c4a81a33b68747ce
le tong <1263512924@qq.com>
2018-10-18 19:34:39 +0800

    update readme

--- FMDBDatabase/Student.h
+++ FMDBDatabase/Student.h
@@ -3,7 +3,7 @@
 //  FMDBDatabase
 //
 //  Created by 李敏 on 2018/4/25.
-//  Copyright © 2018年 Tongle. All rights reserved.
+//  Copyright © 2018年 iOS. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
